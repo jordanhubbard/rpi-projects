2612195631c839274e9ea0f5fbfbfcbb87dc227a
Jordan Hubbard <jordanhubbard@gmail.com>
2024-10-12 21:41:33 -0700

    Add some helper colors.

--- sense-hat/question-mark.py
+++ sense-hat/question-mark.py
@@ -2,8 +2,14 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-X = [255, 0, 0]  # Red
-O = [255, 255, 255]  # White
+red = [255, 0, 0]
+blue = [0, 0, 255]
+green = [0, 255, 0]
+black = [0, 0, 0]
+white = [255, 255, 255]
+
+X = red
+O = white
 
 question_mark = [
 O, O, O, X, X, O, O, O,
