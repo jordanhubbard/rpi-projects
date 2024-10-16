60c3fd48cccfcbdddfb3f84a8986f7e5e913ff30
Jordan Hubbard <jordanhubbard@gmail.com>
2024-10-16 10:38:34 -0700

    Make a better set_pixel / set_pixels example.

--- sense-hat/3.set_pixel.py
+++ sense-hat/3.set_pixel.py
@@ -19,3 +19,14 @@ O, O, O, X, O, O, O, O
 ]
 
 sense.set_pixels(question_mark)
+time.sleep(1)
+
+# What's this?
+sense.set_pixel(2, 2, (0, 0, 255))
+sense.set_pixel(4, 2, (0, 0, 255))
+sense.set_pixel(3, 4, (100, 0, 0))
+sense.set_pixel(1, 5, (255, 0, 0))
+sense.set_pixel(2, 6, (255, 0, 0))
+sense.set_pixel(3, 6, (255, 0, 0))
+sense.set_pixel(4, 6, (255, 0, 0))
+sense.set_pixel(5, 5, (255, 0, 0))
