2f8b57e9bde60bb1c8d623925e0060780608ca97
Jordan Hubbard <jordanhubbard@gmail.com>
2024-10-17 19:12:45 -0700

    Add some prints so it's more interactive. Add a horizontal / vertical flip example.

--- sense-hat/5.flip.py
+++ sense-hat/5.flip.py
@@ -22,11 +22,17 @@ O, O, O, X, O, O, O, O
 sense.set_pixels(question_mark)
 
 
-# Twirl 5 times
+# flip twice in each direction
 i = 0
-while i < 5:
-    for rot in [0, 90, 180, 270]:
-        print(f"Set orientation to {rot} degrees")
-        sense.set_rotation(rot)
-        time.sleep(0.5)
+while i < 4:
+    print("two horizontal flips")
+    sense.flip_h()
+    time.sleep(0.5)
+    sense.flip_h()
+    time.sleep(0.5)
+    print("two vertical flips")
+    sense.flip_v()
+    time.sleep(0.5)
+    sense.flip_v()
+    time.sleep(0.5)
     i += 1
