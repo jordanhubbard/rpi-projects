005674ae22be51e3adb6abc02a4892bd21f12cd5
Jordan Hubbard <jordanhubbard@gmail.com>
2024-10-19 11:51:56 -0700

    Add the accelerometer test case

--- sense-hat/8.IMU_sensor.py
+++ sense-hat/8.IMU_sensor.py
@@ -3,11 +3,12 @@ from colors import Colors
 import time
 
 sense = SenseHat()
-
+iterations = 50
 c = Colors()
+
 print("Orientation in degrees")
 i = 0
-while i < 100:
+while i < iterations:
     orientation_deg = sense.get_orientation_degrees()
     print("p: {pitch}, r: {roll}, y: {yaw}".format(**orientation_deg))
     i += 1
@@ -15,7 +16,7 @@ while i < 100:
 
 print("Compass orientation")
 i = 0
-while i < 100:
+while i < iterations:
     direction = sense.get_compass()
     print("North: %s" % direction)
     match direction:
@@ -29,5 +30,12 @@ while i < 100:
         sense.show_letter('W')
       case _:
         sense.show_message(f"{round(direction,0)}")
+    i += 1
+
+print("Accelerometer test")
+i = 0
+while i < iterations:
+    accel_only = sense.get_accelerometer()
+    print("p: {pitch}, r: {roll}, y: {yaw}".format(**accel_only))
     i += 1
     time.sleep(0.1)
